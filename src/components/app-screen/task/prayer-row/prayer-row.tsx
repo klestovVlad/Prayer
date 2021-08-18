d58895ad4e4468c6b1fef1a9eacdcee6397d840d
klestovVlad <klestov.vlad@gmail.com>
2021-08-18 13:32:26 +0600

    broken swipe

--- src/components/app-screen/task/prayer-row/prayer-row.tsx
+++ src/components/app-screen/task/prayer-row/prayer-row.tsx
@@ -5,6 +5,8 @@ import {UserIcon} from '../../../../ui/Icons/UserIcon';
 import {HandsIcon} from '../../../../ui/Icons/HandsIcon';
 import {colors} from '../../../../style/colors';
 import {Container, VerticalLine, Text, IconsContainer, Number} from './styles';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import {Animated} from 'react-native';
 
 interface PrayerRowProps {
   navigation: any;
@@ -12,19 +14,36 @@ interface PrayerRowProps {
 
 export const PrayerRow: React.FC<PrayerRowProps> = ({navigation}) => {
   const [answered, setanswered] = useState(false);
+
+  const renderRightActions = (
+    proggres: Animated.AnimatedInterpolation,
+    dragX: Animated.AnimatedInterpolation,
+  ) => {
+    const trans = dragX.interpolate({
+      inputRange: [-100, -50, 0, 1],
+      outputRange: [-30, 50, 100, 101],
+    });
+    return (
+      <Animated.View style={{transform: [{translateX: trans}]}}>
+        <Text answered={answered}>Prayer text</Text>
+      </Animated.View>
+    );
+  };
   return (
-    <Container
-      answered={answered}
-      onPress={() => navigation.navigate('Prayer details')}>
-      <VerticalLine />
-      <CheckBox Cheked={answered} ChangeState={setanswered} />
-      <Text answered={answered}>Prayer text</Text>
-      <IconsContainer>
-        <UserIcon />
-        <Number>12</Number>
-        <HandsIcon color={colors.blue} />
-        <Number>12</Number>
-      </IconsContainer>
-    </Container>
+    <Swipeable renderRightActions={renderRightActions} overshootRight={false}>
+      <Container
+        answered={answered}
+        onPress={() => navigation.navigate('Prayer details')}>
+        <VerticalLine />
+        <CheckBox Cheked={answered} ChangeState={setanswered} />
+        <Text answered={answered}>Prayer text</Text>
+        <IconsContainer>
+          <UserIcon />
+          <Number>12</Number>
+          <HandsIcon color={colors.blue} />
+          <Number>12</Number>
+        </IconsContainer>
+      </Container>
+    </Swipeable>
   );
 };
